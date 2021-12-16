36043a0116c84376c3fb45806d271cb8ba9ad84f
Enigmind <enigmind@protonmail.com>
2021-12-16 08:39:25 +0100

    change cat command name

--- commands/pussy.js
+++ commands/pussy.js
@@ -3,8 +3,8 @@ const fetch = require("node-fetch");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("cat")
-    .setDescription("send random and beautiful cats content !"),
+    .setName("pussy")
+    .setDescription("Petit coquinou va..."),
   async execute(interaction) {
     await interaction.deferReply();
     const { file } = await fetch("https://aws.random.cat/meow").then(
