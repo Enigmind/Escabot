39c789f7d101692fcdd93445b1fe9d442282b2cb
Enigmind <enigmind@protonmail.com>
2022-11-18 14:56:51 +0100

    Esca doesn't want to play among us anymore

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
 	execute(client) {
 		// client.channels.cache.get('963435756947382344').send('ratio dim');
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setPresence({ activities: [{ name: 'Among Us' }] });
+		client.user.setPresence({ activities: [{ name: 'La fin du monde', type: "WATCHING" }] });
 	},
 };
