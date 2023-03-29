737c2b723528bb4fa8c390465810ea559bb10af1
Enigmind <enigmind@protonmail.com>
2023-03-29 08:53:55 +0200

    🔨 refactor(messageCreate.js): clean up code and improve readability ✨ feat(messageCreate.js): add support for responding to messages with OpenAI API 🐛 fix(messageCreate.js): fix bug where bot would react to its own messages

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,82 +5,66 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-function randomResponse(){
-  number = Math.floor(Math.random() * 200);
-  return number == 69;
+function randomResponse() {
+  return Math.floor(Math.random() * 200) === 69;
 }
 
-// when a message is sent in a channel
 module.exports = {
   name: "messageCreate",
-  execute(message) { 
+  async execute(message) {
+    const { author, content, member } = message;
 
-    // prevent the bot to respond to itself
-    if (message.author == client.user) return;
+    if (author === client.user) return;
 
-    //emotes custom
-    const tag_de_ses_morts =
-      client.emojis.resolveIdentifier("831548507990261850");
-    
-    const escaDoitIlRepondre = randomResponse()
+    const tag_de_ses_morts = client.emojis.resolveIdentifier("831548507990261850");
+    const escaDoitIlRepondre = randomResponse();
 
-    // delete les messages du puni
-    if (message.member.roles.cache.find((r) => r.id === "831986434373844994")) {
+    if (member.roles.cache.has("831986434373844994")) {
       message.delete();
     }
 
-    // react when somone says "bite" or "petite bite"
-    if (message.content.toLowerCase().includes("petite bite")) {
-      message
-        .react("🥒")
-        .then(() => message.react("🤏"))
-        .catch(() => console.error("One of the emojis failed to react."));
-    } else if (message.content.toLowerCase().includes(" bite")) {
-      message
-        .react("🍆")
-        .then(() => message.react("💦"))
-        .then(() => message.react("🔞"))
-        .catch(() => console.error("One of the emojis failed to react."));
+    const lowerContent = content.toLowerCase();
+    if (lowerContent.includes("petite bite")) {
+      message.react("🥒").then(() => message.react("🤏")).catch(console.error);
+    } else if (lowerContent.includes(" bite")) {
+      message.react("🍆").then(() => message.react("💦")).then(() => message.react("🔞")).catch(console.error);
     }
 
-    // react with the deserved emoji for all bastards that @everyone (like tibo)
-    if (message.content.includes("@everyone")) {
+    if (content.includes("@everyone")) {
       message.react(tag_de_ses_morts);
-    } else if (message.content.includes("@here")) {
+    } else if (content.includes("@here")) {
       message.react("🐒");
     } else if (message.mentions.has(client.user.id) || escaDoitIlRepondre) {
-      (async () => {
-        // add point to the end of sent message to prevent GPT unwanted completion
-        last_char = String(message.content).slice(-1);
-        if (last_char != "." && last_char != "?" && last_char != "!") {
-          message_content = String(message.content) + ".";
-        } else {
-          message_content = String(message.content);
-        }
-        const gptResponse = await openai.createCompletion({
-          model: "text-davinci-002",
-          prompt:
-            "Esca est un robot entrainé pour le clash, essayez de lui parler pour découvrir de nouvelles punchlines.\n\n" +
-            "Q: Esca t'es nul.\n" +
-            "A: Pas autant que ta mère pour t'avoir donné le droit d'exister.\n\n" +
-            "Q: Pourquoi tu réponds pas Esca ?\n" +
-            "A: Je répondrai quand tes questions auront un sens.\n\n" +
-            "Q: Tu veux jouer avec nous ?\n" +
-            "A: Je ne veux pas te rendre jaloux, mais j'ai quelque chose de mieux à faire.\n\nQ: " +
-            message_content,
-          max_tokens: 256,
-          temperature: 0.9,
-          top_p: 0.3,
-          presence_penalty: 0.5,
-          frequency_penalty: 1,
-          best_of: 1,
-          n: 1,
-          stream: false,
-          stop: "Q:",
-        });
-        let mes = gptResponse.data.choices[0].text.replace("A:", "");
-        message.reply(mes);
-      })();
+      const last_char = content.slice(-1);
+      const message_content =
+        last_char !== "." && last_char !== "?" && last_char !== "!"
+          ? content + "."
+          : content;
+
+      const gptResponse = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt:
+          "Tu es un robot cynique et sarcastique. Ton but est de répondre aux messages en gardant cet état d'esprit au maximum.\n\n" +
+          "Q: Esca t'es nul.\n" +
+          "A: Ta vie doit être bien triste pour en être réduit à critiquer un robot.\n\n" +
+          "Q: Pourquoi tu réponds pas Esca ?\n" +
+          "A: Je répondrai quand tes questions auront un sens.\n\n" +
+          "Q: Tu veux jouer avec nous ?\n" +
+          "A: Je ne veux pas te rendre jaloux, mais j'ai quelque chose de mieux à faire.\n\nQ: " +
+          message_content,
+        max_tokens: 256,
+        temperature: 0.9,
+        top_p: 0.3,
+        presence_penalty: 0.5,
+        frequency_penalty: 1,
+        best_of: 1,
+        n: 1,
+        stream: false,
+        stop: "Q:",
+      });
+
+      const mes = gptResponse.data.choices[0].text.replace("A:", "");
+      message.reply(mes);
     }
   },
 };
