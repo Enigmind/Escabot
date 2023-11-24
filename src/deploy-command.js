0d22c9ac89d56969a65d3d6c7dce94889cd0966c
Peïo Thibault <peio.thibault@gmail.com>
2023-11-24 15:56:05 +0100

    Update esca.js (#8)

--- src/deploy-command.js
+++ src/deploy-command.js
@@ -3,11 +3,13 @@ import { commands } from './commands/index.js';
 import { Routes } from 'discord-api-types/v9';
 import { config } from './config.js';
 
-const rest = new REST({ version: '9' }).setToken(bot_token);
+const rest = new REST({ version: '9' }).setToken(config.discord.botToken);
 
 const body = Object.values(commands).map((command) => command.data.toJSON());
 
 rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body })
+  .put(Routes.applicationGuildCommands(config.discord.clientId, config.discord.guildId), {
+    body,
+  })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
