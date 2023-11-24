54b737a60b4e650eb3be3bace1b894e6826a93d8
Peïo Thibault <peio.thibault@gmail.com>
2023-11-24 16:00:14 +0100

    remove log

--- src/config.js
+++ src/config.js
@@ -3,13 +3,6 @@ import { config as dotenv } from 'dotenv';
 
 dotenv();
 
-console.log({
-  openAiApiKey: process.env.OPEN_AI_KEY,
-  clientId: process.env.CLIENT_ID,
-  guildId: process.env.GUILD_ID,
-  botToken: process.env.BOT_TOKEN,
-});
-
 export const config = {
   openAi: {
     apiKey: env.get('OPEN_AI_KEY').required().asString(),
